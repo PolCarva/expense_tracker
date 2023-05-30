b34c1bae9d303fe51581165f36a858d7f0774d86
PolCarva <107940742+PolCarva@users.noreply.github.com>
2023-05-30 15:41:52 -0300

    Creado el proyecto inicial

---
+++ lib/widgets/new_expense.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+class NewExpense extends StatefulWidget {
+  const NewExpense({super.key});
+
+  @override
+  State<NewExpense> createState() => _NewExpenseState();
+}
+
+class _NewExpenseState extends State<NewExpense> {
+  final _titleControler = TextEditingController();
+  final _amountControler = TextEditingController();
+
+
+  @override
+  void dispose() {
+    _titleControler.dispose();
+    _amountControler.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        children: [
+          TextField(
+            maxLength: 50,
+            controller: _titleControler,
+            decoration: const InputDecoration(
+              label: Text("Title"),
+            ),
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  keyboardType: TextInputType.number,
+                  controller: _amountControler,
+                  decoration: const InputDecoration(
+                    prefixText: "\$ ",
+                    label: Text("Amount"),
+                  ),
+                ),
+              ),
+              const SizedBox(width: 16),
+              Expanded(
+                  child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Text("Selected Date"),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.calendar_month),
+                  )
+                ],
+              ))
+            ],
+          ),
+          Row(
+            children: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text("Cancel"),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  print(_titleControler.text);
+                  print(_amountControler.text);
+                },
+                child: const Text("Save Expense"),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
